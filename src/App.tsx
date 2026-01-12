09afb0b0faecde62ff7b4c328c066b94b7651ba4
Mathijs Duisdecker <kommaconsult@outlook.com>
2026-01-12 14:21:42 +0100

    Add Vercel Analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Diensten from './pages/Diensten'
@@ -7,14 +8,17 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/diensten" element={<Diensten />} />
-        <Route path="/over" element={<Over />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Layout>
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/diensten" element={<Diensten />} />
+          <Route path="/over" element={<Over />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+      <Analytics />
+    </>
   )
 }
 
